44be00bd57b0a8c9b5ade4765a5d666841a607c4
IoannisMorakis <62579052+IoannisMorakis@users.noreply.github.com>
2021-10-21 11:40:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const searchBox = document.querySelector(".search-box");
-const navBtnContainer = document.querySelector(".nav-btn-container");
-const searchBtn = document.querySelector(".search-btn");
-const closeBtn = document.querySelector(".close-btn");
-
-searchBtn.addEventListener("click", () => {
-  searchBox.classList.add("active");
-  navBtnContainer.classList.add("active");
-});
-
-closeBtn.addEventListener("click", () => {
-  searchBox.classList.remove("active");
-  navBtnContainer.classList.remove("active");
-});
 
 function myFunction() {
   ;
