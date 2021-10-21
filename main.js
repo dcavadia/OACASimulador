d5b8fb875263316ee2a366abe674163876fd0e39
IoannisMorakis <62579052+IoannisMorakis@users.noreply.github.com>
2021-10-21 17:00:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const porcen_perdida_phytophthora = 0.8;
 const porcen_perdida_ralstonia = 0.8;
 const porcen_perdida_xanthomonas = 0.7;
 const porcen_perdida_alternaria = 0.3;
+const densidad1= 2;
+const densidad2= 2.5;
 
 
 function CantidadPlantas(densidad, metros_cuadrados, porcen_germinacion) {
@@ -12,10 +14,40 @@ function CantidadProductos(cant_plantas, rendimiento) {
     return (cant_plantas * rendimiento);
 }
 
-function CantidadPerdidas(porcen_perdida, cant_plantas) {
-    return (porcen_perdida * cant_plantas)
+function CantidadPerdidas(porcen_perdida, cant_plantas, rendimiento) {
+    return (porcen_perdida * cant_plantas * rendimiento)
 }
 
 function CantidadProductosPatogeno(cant_producto, cant_perdida) {
     return (cant_producto - cant_perdida)
+}
+
+
+function myFunction() {
+  var x = document.getElementById("CuadRes");
+  x.hidden = false;
+  var a = document.getElementById("Area").value;
+  var por = document.getElementById("Porcentaje").value;
+  var pat = document.getElementById("Patógeno").value;
+  
+  var res1_a= CantidadPlantas(densidad1, a, por);
+  var res2_a= CantidadProductos(res1_a, 3); // 1-3
+  var porcen_perdida= 0.8;
+
+  if(pat=="Xanthomonas Vesicatoria"){porcen_perdida= 0.7;}
+  if(pat=="Alternaria solani"){porcen_perdida= 0.3;}
+  var res3_a= CantidadProductos(porcen_perdida, res1_a, 3);
+  var res4_a= CantidadProductos(res1_a, res3_a);
+  
+
+  var old= document.getElementById("myRes1").innerHTML;
+  document.getElementById("myRes1").innerHTML= old + "     " + res1_a ;
+  var old= document.getElementById("myRes2").innerHTML;
+  document.getElementById("myRes2").innerHTML= old + "     " + res2_a + " Kg";
+  var old= document.getElementById("myRes3").innerHTML;
+  document.getElementById("myRes3").innerHTML= old + "                  " + res3_a + "Kg";
+  var old= document.getElementById("myRes4").innerHTML;
+  document.getElementById("myRes4").innerHTML= old + res4_a + "Kg";
+
+  //document.getElementById("p1").innerHTML = "New text!";
 }
